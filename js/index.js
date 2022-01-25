0a48f3c8803da6d55edbbd3f669cabe84badbc2d
Mauricio Saizar <msaizar77@hotmail.com>
2022-01-25 17:55:26 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,38 +1,44 @@
-var botonEncriptar = document.getElementById("botonEncriptar");
-var botonDesencriptar = document.getElementById("botonDesencriptar");
-var botonCopiarResultado = document.getElementById("botonCopiarResultado");
+var botonEncriptar = document.getElementById("btn-encriptar");
+var botonDesencriptar = document.getElementById("btn-desencriptar");
+var botonCopiarResultado = document.getElementById("btn-copy");
 
-botonEncriptar.addEventListener("click",function(){
+botonEncriptar.addEventListener("click",function(event){
+    
+    event.preventDefault();
 
-    var aCodificar = document.getElementById("aCodificar");
+    var aCodificar = document.getElementById("input-texto");
 
     if (!validarEntrada(aCodificar.value)) return;
 
     var resultado = encriptarTexto(aCodificar.value);
 
-    var textResultado = document.getElementById("textResultado");
+    var textResultado = document.getElementById("msg");
     textResultado.value = resultado;
 
     aCodificar.value = "";    
 
 })
 
-botonDesencriptar.addEventListener("click",function(){
+botonDesencriptar.addEventListener("click",function(event){
+    
+    event.preventDefault();
 
-    var aCodificar = document.getElementById("aCodificar");
+    var aCodificar = document.getElementById("input-texto");
     var resultado = desencriptarTexto(aCodificar.value);
 
-    var textResultado = document.getElementById("textResultado");
+    var textResultado = document.getElementById("msg");
     textResultado.value = resultado;
     
     aCodificar.value = "";    
 
 })
 
-botonCopiarResultado.addEventListener("click",function(){
+botonCopiarResultado.addEventListener("click",function(event){
 
-    var aCodificar = document.getElementById("aCodificar");
-    var textResultado = document.getElementById("textResultado");
+    event.preventDefault();
+    
+    var aCodificar = document.getElementById("input-texto");
+    var textResultado = document.getElementById("msg");
 
     var txtEncriptado = textResultado.value;
     navigator.clipboard.writeText(txtEncriptado);
